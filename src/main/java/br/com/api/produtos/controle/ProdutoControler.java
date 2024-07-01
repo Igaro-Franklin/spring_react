cb71fbaa8ae05f09f44d6624ccf7da42b62ecc66
Igaro-Franklin <igarofranklin@gmail.com>
2024-07-01 17:10:51 -0300

    Iniciou o projeto de java(springboot) com reactjs

---
+++ src/main/java/br/com/api/produtos/controle/ProdutoControler.java
@@ -0,0 +1,28 @@
+package br.com.api.produtos.controle;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class ProdutoControler {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+        return ps.listar();
+    }
+    
+    
+    @GetMapping("/")
+    public String rota(){
+        return "API de produtos funcionando agora!";
+    }
+}
